51da38989298c6afc6be249737ea3d5442fb79e0
Brian Chang <bchang@guidewire.com>
2011-10-04 21:02:16 -0700

    added target for internal staging web server

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 SOURCE=build/www/
 TARGET_LOCAL=/mnt/sdb2/home/bchang/gosu-lang
+TARGET_INTERNAL=root@opensource-internal:/var/www/html/gosu
 TARGET=$TARGET_LOCAL
 
 rsync --recursive --checksum --perms --verbose --delete --rsh=ssh --exclude-from=rsync-exclude --dry-run $SOURCE $TARGET
